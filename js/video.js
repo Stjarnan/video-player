1a58af44435391edf6073e5bc7ce74ba29fd4666
Stjarnan <Stjarnan@Jonass-MacBook-Pro.local>
2016-07-15 01:20:09 +0200

    More buttons added
    Added more buttons, no functionality or icons added to them yet.

--- js/video.js
+++ js/video.js
@@ -1,13 +1,15 @@
 
-var playPausebtn, video, progress, currentTime, totalTime;
+var playPausebtn, video, progressbar, curTime, totTime, volumeControl, volumeRange, fullscreen, videoPlayer, forward10, rewind10;
 
 
 // Video functions
 
+
+	// Function for the progressbar
 function progress() {
 	// Get and update time in slider
 	var timeUpdate = video.currentTime * (100 / video.duration);
-
+    progressbar.value = timeUpdate;
 	// Get time for the "clock" in the video player
 	var currentMin = Math.floor(video.currentTime / 60);
 	var currentSec = Math.floor(video.currentTime - currentMin *  60);
@@ -27,25 +29,80 @@ function progress() {
 		totalMin = "0" + totalMin;
 	}
 
- 	currentTime.innerHTML = currentMin + ":" + currentSec;
- 	totalTime.innerHTML = totalMin + ":" + totalSec;
+ 	curTime.innerHTML = currentMin + ":" + currentSec;
+ 	totTime.innerHTML = totalMin + ":" + totalSec;
 
 }
-
+	
+	// Function for the play/pause button
 function playPause() {
 					if(video.paused){
 						video.play();
+						playPausebtn.style.background = "url(icons/pause-icon.png)";
+						playPausebtn.style.backgroundRepeat = "no-repeat"; 
+						playPausebtn.style.backgroundSize = "cover";     
+    					playPausebtn.style.backgroundPosition = "center";
 					}	else {
 						video.pause();
+						playPausebtn.style.background = "url(icons/play-icon.png)";
 					}
 }
 
+	//function to enable the progressbar to be adjusted by user
 function progresSlide(){
 	var goTo = video.duration * (progressbar.value / 100);
 	video.currentTime = goTo;
 }
+	
+	// Mute/unmute video when clicking the volume-icon
+function mute(){
+	if (video.muted){
+		video.muted = false;
+		volumeRange.value = 100;
+		volumeControl.style.background = "url(icons/volume-on-icon.png)"; 
+		volumeControl.style.backgroundRepeat = "no-repeat"; 
+		volumeControl.style.backgroundSize = "cover";     
+		volumeControl.style.backgroundPosition = "center";
+	} else {
+		video.muted = true;
+		volumeRange.value = 0;
+		volumeControl.style.background = "url(icons/volume-off-icon.png)"; 
+		volumeControl.style.backgroundRepeat = "no-repeat"; 
+		volumeControl.style.backgroundSize = "cover";     
+		volumeControl.style.backgroundPosition = "center";
+	}
+}
 
+	// The Volumebar 
+function volumeChange() {
+	video.volume = volumeRange.value / 100;
+}
 
+	// enter fullscreen when clicking the button
+function goFullscreen(){
+	if(videoPlayer.requestFullscreen){
+		videoPlayer.requestFullscreen()
+		videoPlayer.style.maxWidth = "none";
+			} 
+	else if (videoPlayer.webkitRequestFullscreen) {
+		videoPlayer.webkitRequestFullscreen();
+		videoPlayer.style.maxWidth = "none";
+		
+	} else {
+		videoPlayer.mozRequestFullscreen();	
+		videoPlayer.style.maxWidth = "none";	
+	}
+}	
+
+	// rewind 10 seconds
+function rewind() {
+	video.currentTime -= 10 ; 
+}
+
+	// forward 10 seconds
+function forward() {
+	video.currentTime += 10;
+}
 
 
 // Bind the html to the JavaScript
@@ -56,13 +113,24 @@ function loadPlayer() {
 	playPausebtn = document.getElementById("play");
 	video = document.getElementById("video");
 	progressbar = document.getElementById("progressbar");
-	currentTime = document.getElementById("currentTime");
-	totalTime = document.getElementById("totalTime");
+	curTime = document.getElementById("currentTime");
+	totTime = document.getElementById("totalTime");
+	volumeControl = document.getElementById("volumeControl");
+	volumeRange = document.getElementById("volumeRange");
+	fullscreen = document.getElementById("fullscreen");
+	videoPlayer = document.getElementById("video-player");
+	forward10 = document.getElementById("forward10");
+	rewind10 = document.getElementById("rewind10");
 
 	// Add eventlisteners for the buttons in the video controlbar
 	video.addEventListener("timeupdate", progress);
 	playPausebtn.addEventListener("click", playPause);
 	progressbar.addEventListener("change", progresSlide);
+	volumeControl.addEventListener("click", mute);
+	volumeRange.addEventListener("change", volumeChange);
+	fullscreen.addEventListener("click", goFullscreen);
+	forward10.addEventListener("click", forward);
+	rewind10.addEventListener("click", rewind);
 
 }
 
